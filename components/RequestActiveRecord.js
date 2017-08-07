a811782ff8ad6f12fe584e87147bf977092f682b
Yuichiro Take <y.takey@gmail.com>
2017-08-07 12:47:21 +0900

    commit

--- components/RequestActiveRecord.js
+++ components/RequestActiveRecord.js
@@ -1,43 +1,45 @@
-import _ from "lodash";
-import React, { Component } from "react";
+import _ from 'lodash';
+import React, { Component } from 'react';
+import StyledRow from './StyledRow';
 
-const labelStyle = { fg: "magenta", bold: true };
-const durationStyle = { fg: "yellow" };
+const typeStyle = { fg: 'magenta', bold: true, align: 'right' };
+const durationStyle = { fg: 'yellow' };
+const sqlStyle = {};
+const durationHeaderStyle = { fg: 'yellow' };
+const sqlHeaderStyle = { fg: 'magenta' };
 
 const format = num => {
-  const str = String(num).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+  const str = String(num).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
   return `${str.padStart(7)} ms`;
+};
+
+const containerStyle = {
+  padding: { top: 1, left: 2, right: 2 }
 };
 
 class RequestActiveRecord extends Component {
   render() {
     const { sqlTime, activeRecords } = this.props.data;
-    const maxTypeLength = _.max(activeRecords.map(({ type }) => type.length)) + 1;
+    const maxTypeLength = _.max(activeRecords.map(({ type }) => type.length));
 
     return (
-      <box top={0} height="100%-2" left={0} width="100%-2">
-        Total {format(sqlTime)}, {activeRecords.length} SQL
-        {activeRecords.map(({ type, sql, duration }, i) => {
-          return [
-            <box
-              top={i + 1}
-              height={1}
-              left={0}
-              width={maxTypeLength}
-              content={type.padStart(maxTypeLength)}
-              style={labelStyle}
-            />,
-            <box
-              top={i + 1}
-              height={1}
-              left={maxTypeLength + 1}
-              width={10}
-              content={format(duration)}
-              style={durationStyle}
-            />,
-            <box top={i + 1} height={1} left={maxTypeLength + 11} width={`100%-${maxTypeLength + 11}`} content={sql} />
-          ];
-        })}
+      <box top={0} height="100%-2" left={0} width="100%-6" {...containerStyle}>
+        <StyledRow
+          top={0}
+          columns={['Total', format(sqlTime), `${activeRecords.length} SQL`]}
+          styles={[{ width: maxTypeLength, align: 'right' }, durationHeaderStyle, sqlHeaderStyle]}
+        />
+
+        <StyledRow top={1} columns={['================']} styles={[{ fg: 'white' }]} />
+
+        {activeRecords.map(({ type, duration, sql }, i) =>
+          <StyledRow
+            key={`styledrow-${i}`}
+            top={i + 2}
+            columns={[type, format(duration), sql]}
+            styles={[{ width: maxTypeLength, ...typeStyle }, durationStyle, sqlStyle]}
+          />
+        )}
       </box>
     );
   }
---
+++ components/StyledRow.js
@@ -0,0 +1,31 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+
+class StyledRow extends Component {
+  render() {
+    const { top, styles, columns, spacer = 1 } = this.props;
+    styles.reduce((left, style, i) => {
+      style.left = left;
+      if (!style.width) {
+        style.width = i === columns.length - 1 ? `100%-${left}` : columns[i].length;
+      }
+
+      return left + style.width + spacer;
+    }, 0);
+
+    return (
+      <box top={top} height={1} left={0} width="100%">
+        {columns.map((column, i) =>
+          <box
+            key={`row-${top}-${i}`}
+            {..._.pick(styles[i], 'left', 'width', 'align')}
+            content={column}
+            style={styles[i]}
+          />
+        )}
+      </box>
+    );
+  }
+}
+
+export default StyledRow;
