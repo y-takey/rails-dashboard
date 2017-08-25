a7d8c36553d9c002d20e6fffe430d7ccb9657ff7
Yuichiro Take <y.takey@gmail.com>
2017-08-25 12:51:05 +0900

    update readme

--- bin/rails-dashboard.js
+++ bin/rails-dashboard.js
@@ -8,11 +8,34 @@ const pkg = require("../package.json");
 const program = new commander.Command("rails-dashboard");
 
 program.version(pkg.version);
-program.option("-c, --color [color]", "Dashboard color");
-program.option("-m, --minimal", "Minimal mode");
-program.option("-t, --title [title]", "Terminal window title");
-program.option("-p, --port [port]", "Socket listener port");
-program.usage("[options] -- [script] [arguments]");
+// program.option("-c, --color [color]", "Dashboard color");
+program.usage("[options] -- [rails command] [arguments]");
+program.on("--help", () => {
+  [
+    "",
+    "  Examples:",
+    "",
+    "    $ rails-dashboard bin/rails s",
+    "",
+    "  Key Map:",
+    "",
+    "    # Cursor navigation",
+    "        <j> or <Down> : move down by 1 row",
+    "          <k> or <Up> : move up by 1 row",
+    "              <Space> : move down by 1 page",
+    "    <Shift> + <Space> : move up by 1 page",
+    "                  <g> : move to top",
+    "        <Shift> + <g> : move to bottom",
+    "    # View manipulation",
+    "              <Enter> : show detail about selected row",
+    "             <Escape> : hide the detail",
+    "                  <b> : show detail with Breakdown tab",
+    "                  <p> : show detail with Params tab",
+    "                  <a> : show detail with ActiveRecord tab",
+    "                  <r> : show detail with Rendering tab",
+    "                  <l> : show detail with Log tab"
+  ].forEach(line => console.log(line));
+});
 program.parse(process.argv);
 
 const command = program.args[0];
