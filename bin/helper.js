4d13db56a0501adb87328f4b415e77d4167bea14
takeyuichi <y.takey@gmail.com>
2017-09-03 15:17:46 +0900

    Improve help message format

---
+++ bin/helper.js
@@ -0,0 +1,57 @@
+const _ = require("lodash");
+const chalk = require("chalk");
+
+const wrapKey = key => chalk.cyan(`<${key}>`);
+
+const caption = keys => {
+  if (_.isString(keys)) return wrapKey(keys);
+  return keys[0].map(wrapKey).join(` ${keys[1]} `);
+};
+
+const ANSIRegex = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
+
+const pad = str => {
+  const ansiLength = str.length - str.replace(ANSIRegex, "").length;
+  return _.padStart(str, 23 + ansiLength);
+};
+
+const foo = ([keys, desc]) => {
+  return `${pad(caption(keys))} : ${desc}`;
+};
+
+const cursorNavKeys = [
+  [[["j", "Down"], "or"], "move down by 1 row"],
+  [[["k", "Up"], "or"], "move up by 1 row"],
+  ["Space", "move down by 1 page"],
+  [[["Shift", "Space"], "+"], "move up by 1 page"],
+  ["g", "move to top"],
+  [[["Shift", "g"], "+"], "move to bottom"]
+];
+
+const viewManipulationKeys = [
+  ["Enter", "show detail about selected row"],
+  ["Escape", "hide the detail"],
+  ["b", "show detail with Breakdown tab"],
+  ["p", "show detail with Params tab"],
+  ["a", "show detail with ActiveRecord tab"],
+  ["r", "show detail with Rendering tab"],
+  ["l", "show detail with Log tab"]
+];
+
+module.exports = () => {
+  [
+    "",
+    "  Examples:",
+    "",
+    `    $ ${chalk.cyan("rails-dashboard bin/rails s")}`,
+    "",
+    "  Key Map:",
+    "",
+    `    ${chalk.yellow("# Cursor navigation")}`,
+    ...cursorNavKeys.map(foo),
+    "",
+    `    ${chalk.yellow("# View manipulation")}`,
+    ...viewManipulationKeys.map(foo),
+    ""
+  ].forEach(line => console.log(line));
+};
--- bin/rails-dashboard.js
+++ bin/rails-dashboard.js
@@ -7,37 +7,13 @@ const main =
   process.env.NODE_ENV === "dev" || process.env.NODE_ENV === "dmy" ? require("../src/main") : require("../lib/main");
 const pkg = require("../package.json");
 const program = new commander.Command("rails-dashboard");
+const helper = require("./helper");
 const updateCheck = require("./self-update-checker");
 
 program.version(pkg.version);
 // program.option("-c, --color [color]", "Dashboard color");
 program.usage("[options] -- [rails command] [arguments]");
-program.on("--help", () => {
-  [
-    "",
-    "  Examples:",
-    "",
-    "    $ rails-dashboard bin/rails s",
-    "",
-    "  Key Map:",
-    "",
-    "    # Cursor navigation",
-    "        <j> or <Down> : move down by 1 row",
-    "          <k> or <Up> : move up by 1 row",
-    "              <Space> : move down by 1 page",
-    "    <Shift> + <Space> : move up by 1 page",
-    "                  <g> : move to top",
-    "        <Shift> + <g> : move to bottom",
-    "    # View manipulation",
-    "              <Enter> : show detail about selected row",
-    "             <Escape> : hide the detail",
-    "                  <b> : show detail with Breakdown tab",
-    "                  <p> : show detail with Params tab",
-    "                  <a> : show detail with ActiveRecord tab",
-    "                  <r> : show detail with Rendering tab",
-    "                  <l> : show detail with Log tab"
-  ].forEach(line => console.log(line));
-});
+program.on("--help", helper);
 program.parse(process.argv);
 
 const command = program.args[0];
